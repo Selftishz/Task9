db4485f640a001d02ad7b020601349a15e95d2df
Artyom <Artyom@DESKTOP-HB4QEA4>
2022-05-24 23:52:15 +0300

    Добавьте файлы проекта.

---
+++ Task9/Header.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef HEAD_H
+#define HEAD_H
+using namespace std;
+#include <iostream>
+class Property
+{
+private:
+
+    int worth{};
+public:
+    Property(int = 0);
+
+    int getWorth() const {
+        return worth;
+    }
+
+    void setWorth(int worth) {
+        Property::worth = worth;
+    }
+
+    virtual int tax() = 0;
+};
+class Apartment : public Property {
+public:
+    Apartment(int a);
+
+    int tax();
+};
+class Car :public Property {
+public:
+    Car(int a);
+    int tax();
+};
+class CountryHouse :public Property {
+public:
+    CountryHouse(int a);
+
+    int tax();
+};
+#endif
